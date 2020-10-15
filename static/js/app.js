d2b7c671e4e06cf623d4ebb4bd3d53292e81c98a
Jakob Voss <voss@gbv.de>
2020-10-15 14:48:35 +0200

    Check whether user can edit

--- static/js/app.js
+++ static/js/app.js
@@ -826,14 +826,28 @@ const app = {
     return {
       login: 'coli-conc.gbv.de/login/',
       user: null,
+      userCanAdd: false,
       auth: null
     }
   },
   methods: {
     updateUser (user) {
       this.user = user
+      this.checkAuth()
     },
-    updateAuth (auth) { this.auth = auth }
+    updateAuth (auth) {
+      this.auth = auth
+      return this.checkAuth()
+    },
+    checkAuth () {
+      if (this.auth) {
+        const url = '/api/checkAuth?type=schemes&action=create'
+        const headers = { Authorization: `Bearer ${this.auth.token}` }
+        fetch(url, { headers }).then(res => { this.userCanAdd = res.ok })
+      } else {
+        this.userCanAdd = false
+      }
+    }
   }
 }
 
