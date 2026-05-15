6739de9b6c9f50e9cac55b54f3a04cb3cbfce370
Rodolfo Marraffa <rodolfo.marraffa@gbv.de>
2026-05-15 14:55:19 +0200

    Migrate to composition API for UserStatus; add tests #287

--- vue/components/UserStatus.vue
+++ vue/components/UserStatus.vue
@@ -13,85 +13,84 @@
   </a>
 </template>
 
-<script>
+<script setup>
+import { computed, reactive, ref } from "vue"
 import { LoginClient } from "gbv-login-client"
 
 /**
  * Shows and manages login status.
  */
-export default {
-  name: "UserStatus",
-  props: {
-    login: {
-      type: Object,
-      required: true,
-    },
+defineOptions({ name: "UserStatus" })
+
+const props = defineProps({
+  login: {
+    type: Object,
+    required: true,
   },
-  emits: ["update:user", "update:auth"],
-  data () {
-    return {
-      client: new LoginClient(this.login.api, { ssl: this.login.ssl }),
-      connected: false,
-      user: null,
-      auth: {},
-      error: null,
-    }
-  },
-  computed: {
-    loginUrl() {
-      let url = `http${this.login.ssl ? "s" : ""}://${this.login.api}`
-      if (!url.endsWith("/")) {
-        url += "/"
-      }
-      if (this.user) {
-        url += "account"
-      } else {
-        url += "login?redirect_uri="
-        url += encodeURIComponent(window.location.href)
-      }
-      return url
-    },
-  },
-  created () {
-    const { connect, disconnect, login, logout, update, token, about, error } = LoginClient.events
-    this.client.addEventListener(about, ({ publicKey }) => {
-      this.auth.publicKey = publicKey; this._updateAuth()
-    })
-    this.client.addEventListener(connect, () => {
-      this.connected = true
-    })
-    this.client.addEventListener(disconnect, () => {
-      this.connected = false
-    })
-    this.client.addEventListener(login, ({ user }) => {
-      this._setUser(user)
-    })
-    this.client.addEventListener(update, ({ user }) => {
-      this._updadeUser(user)
-    })
-    this.client.addEventListener(logout, () => {
-      this._setUser(null)
-    })
-    this.client.addEventListener(token, ({ token }) => {
-      this.auth.token = token; this._updateAuth()
-    })
-    this.client.addEventListener(error, (e) => {
-      if (e.error instanceof LoginClient.errors.ThirdPartyCookiesBlockedError) {
-        this.error = "Login is not possible because third-party cookies are blocked."
-      } else {
-        this.error = "Login is not possible because connection failed."
-      }
-    })
-    this.client.connect()
-  },
-  methods: {
-    _updateAuth () {
-      this.$emit("update:auth", this.auth.token ? this.auth : null)
-    },
-    _setUser (user) {
-      this.user = user
-      this.$emit("update:user", user)
-    },
-  },
+})
+
+const emit = defineEmits(["update:user", "update:auth"])
+
+const connected = ref(false)
+const user = ref(null)
+const auth = reactive({})
+const error = ref(null)
+const client = new LoginClient(props.login.api, { ssl: props.login.ssl })
+
+const loginUrl = computed(() => {
+  let url = `http${props.login.ssl ? "s" : ""}://${props.login.api}`
+  if (!url.endsWith("/")) {
+    url += "/"
+  }
+  if (user.value) {
+    url += "account"
+  } else {
+    url += "login?redirect_uri="
+    url += encodeURIComponent(window.location.href)
+  }
+  return url
+})
+
+function updateAuth() {
+  emit("update:auth", auth.token ? { ...auth } : null)
 }
+
+function setUser(nextUser) {
+  user.value = nextUser
+  emit("update:user", nextUser)
+}
+
+const { connect, disconnect, login: loginEvent, logout, update, token, about, error: errorEvent } = LoginClient.events
+
+client.addEventListener(about, ({ publicKey }) => {
+  auth.publicKey = publicKey
+  updateAuth()
+})
+client.addEventListener(connect, () => {
+  connected.value = true
+})
+client.addEventListener(disconnect, () => {
+  connected.value = false
+})
+client.addEventListener(loginEvent, ({ user }) => {
+  setUser(user)
+})
+client.addEventListener(update, ({ user }) => {
+  setUser(user)
+})
+client.addEventListener(logout, () => {
+  setUser(null)
+})
+client.addEventListener(token, ({ token }) => {
+  auth.token = token
+  updateAuth()
+})
+client.addEventListener(errorEvent, (event) => {
+  if (event.error instanceof LoginClient.errors.ThirdPartyCookiesBlockedError) {
+    error.value = "Login is not possible because third-party cookies are blocked."
+  } else {
+    error.value = "Login is not possible because connection failed."
+  }
+})
+client.connect()
 </script>
